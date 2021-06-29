d9f30572a144e5211e2b29ab94374e05791c2457
Alexandre Nogueira <alexandre.nogueira@outlook.com>
2021-06-29 14:28:24 -0300

    commit novo

--- src/View/PlayerView.cs
+++ src/View/PlayerView.cs
@@ -5,11 +5,6 @@ using DinoGame.Models;
 namespace DinoGame.Views{
     public class PlayerView : MainGameView{
     public string playerNameLabel;
-    // { 
-    //     get { return playerNameLabel; } 
-    //     set { playerNameLabel  = value; } 
-    // }
-
 
     //Create new player screen
         public string GetPlayerName(){
@@ -17,16 +12,16 @@ namespace DinoGame.Views{
             Console.Write("Nome do Jogador " + playerNameLabel + " : ");
             return Console.ReadLine();
         }
+
     //Display player data
         public void ShowPlayer(Player player){
             ScreenUtility.WriteColoredString("Jogador " + player.GetName(), ConsoleColor.Black, ConsoleColor.DarkYellow);
         }
 
-        
         public void ShowPlayerSub(Player player){
             ShowPlayer(player);
-            ScreenUtility.WriteColoredString(("").PadRight(40, '-'), ConsoleColor.Black, ConsoleColor.DarkYellow);
+            //ScreenUtility.WriteColoredString(("").PadRight(40, '-'), ConsoleColor.Black, ConsoleColor.DarkYellow);
+            Console.WriteLine();
         }
-
     }
 }
